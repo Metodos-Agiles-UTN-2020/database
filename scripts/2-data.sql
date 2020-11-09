129c8fccbfc7e607417ff689d5513cf29a2092c9
Federico Comelli <FefeJoker.28@gmail.com>
2020-11-09 20:07:14 -0300

    Crear tablas modelo de datos Licencia y TipoLicencia

--- scripts/2-data.sql
+++ scripts/2-data.sql
@@ -3,3 +3,43 @@
 INSERT INTO public.application_user(
 	id, apellido, dni, mail, nombre, password, tipo_usuario, username)
 	VALUES (1, 'Usuario', '12345678', 'user@domain.com', 'Super', '$2a$10$Bu31TZkrrToo6YXWAOBhV.HW0mQ3maMPW76ic3HbnWokqFIp5VxKC', 1, 'user');
+
+INSERT INTO public.tipo_licencia(
+	id, codigo, edad_maxima, edad_minima, licencia_profesional)
+	VALUES (0, 0, NULL, 17, false);
+
+INSERT INTO public.tipo_licencia(
+	id, codigo, edad_maxima, edad_minima, licencia_profesional)
+	VALUES (1, 1, NULL, 17, false);
+
+INSERT INTO public.tipo_licencia(
+	id, codigo, edad_maxima, edad_minima, licencia_profesional)
+	VALUES (2, 2, 65, 21, true);
+
+INSERT INTO public.tipo_licencia(
+	id, codigo, edad_maxima, edad_minima, licencia_profesional)
+	VALUES (3, 3, 65, 21, true);
+
+INSERT INTO public.tipo_licencia(
+	id, codigo, edad_maxima, edad_minima, licencia_profesional)
+	VALUES (4, 4, 65, 21, true);
+
+INSERT INTO public.tipo_licencia(
+	id, codigo, edad_maxima, edad_minima, licencia_profesional)
+	VALUES (5, 5, NULL, 17, false);
+
+INSERT INTO public.tipo_licencia(
+	id, codigo, edad_maxima, edad_minima, licencia_profesional)
+	VALUES (6, 6, NULL, 17, false);
+
+INSERT INTO public.tipo_licencia_licencias_necesarias(
+	tipo_licencia_id, licencias_necesarias_id)
+	VALUES (2, 1);
+
+INSERT INTO public.tipo_licencia_licencias_necesarias(
+	tipo_licencia_id, licencias_necesarias_id)
+	VALUES (3, 1);
+
+INSERT INTO public.tipo_licencia_licencias_necesarias(
+	tipo_licencia_id, licencias_necesarias_id)
+	VALUES (4, 1);
